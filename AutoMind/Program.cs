feaa8414eae79482b1ccbba78850a8a7d302c970
Mimm <Mimm@AMAZINGPC>
2023-01-29 22:18:41 +0300

    Добавьте файлы проекта.

---
+++ AutoMind/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoMind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ AutoMind/Variable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMind
+{
+    internal class Variable
+    {
+        public string Name { get; set; }
+        public string View { get; set; }
+    }
+}

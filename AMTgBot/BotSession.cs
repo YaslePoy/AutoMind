75705259800e6fe8b5785932bfe12152cb5a0f64
Михаил Митрофанов <mmm606@inbox.ru>
2023-02-15 10:36:21 +0300

    Adding bot session

---
+++ AMTgBot/BotSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace AMTgBot
+{
+    internal class BotSession
+    {
+        public long ChatId;
+        public virtual void NextMessage(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken) { }
+
+    }
+}
